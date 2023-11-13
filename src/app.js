af48ed60ccdca064a03c8ba2ce208bf61ebd0717
Swasti008 <144793835+Swasti008@users.noreply.github.com>
2023-11-13 14:34:04 +0530

    project done

--- src/app.js
+++ src/app.js
@@ -26,6 +26,19 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
+    for(let j=0;j<players.length;j++){
+        detailedPlayers[j] = {
+            name: `${players[j]}`,
+            image: `./images/super-${j+1}.png`,
+            strength: getRandomStrength()
+        }
+        if (j%2==0){
+            detailedPlayers[j].type ="hero";
+        }
+        else{
+            detailedPlayers[j].type ="villain";
+        }
+    }
 
     return detailedPlayers;
 }
@@ -34,6 +47,7 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return Math.ceil(Math.random()*100);
 }
 
 const buildPlayers = (players, type) => {
@@ -42,6 +56,16 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
+    for (let j=0;j<players.length;j++){
+        if(players[j].type === type){
+            fragment +=`<div class="player">
+             <img src="${players[j].image}" alt="">
+             <div class="name">${players[j].name}</div>
+             <div class="strength">${players[j].strength}</div>
+            </div> `;
+
+        }
+    }
 
     return fragment;
 }
